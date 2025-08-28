4316e2feb2329046c3ba7497e60c768fe476623f
Olga <o.gavrikova877@gmail.com>
2025-08-28 13:35:02 +0400

    settings.py edited

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -135,6 +135,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [BASE_DIR / 'locale']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
